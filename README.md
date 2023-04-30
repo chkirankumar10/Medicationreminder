4fc7465af6f2dce17fc8e9cd4ae0a3ec2951d179
kirankumar <kirankkspl@gmail.com>
2023-04-30 10:46:04 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Medical Reminder App
+
+This is a mobile app for elderly people above 65 years with a use case of medical reminder built using HTML, CSS, and JavaScript. The app allows users to add medications and reminders, and displays a list of added medications and reminders.
+
+### Features
+
+Add and remove medications
+Add and remove reminders
+Set reminder times for each medication
+Display medication and reminder lists
+
+### Usage
+
+To add a medication, fill out the medication form with the medication name and dosage and click the "Add Medication" button. The medication will be added to the medication list.
+
+To add a reminder, fill out the reminder form with the reminder name and time and click the "Add Reminder" button. The reminder will be added to the reminder list.
+
+To view the list of added medications and reminders, simply scroll down to the corresponding section of the app.
+
+### Technologies Used
+
+This app was built using HTML, CSS, and JavaScript.
---
+++ index.js
@@ -0,0 +1,27 @@
+const medicationForm = document.getElementById("medication-form");
+medicationForm.addEventListener("submit", addMedication);
+
+function addMedication(e) {
+  e.preventDefault();
+  const medicationName = document.getElementById("medication-name").value;
+  const medicationDosage = document.getElementById("medication-dosage").value;
+  const medicationList = document.getElementById("medication-list");
+  const li = document.createElement("li");
+  li.innerText = `${medicationName} - ${medicationDosage}`;
+  medicationList.appendChild(li);
+  medicationForm.reset();
+}
+
+const reminderForm = document.getElementById("reminder-form");
+reminderForm.addEventListener("submit", addReminder);
+
+function addReminder(e) {
+  e.preventDefault();
+  const reminderName = document.getElementById("reminder-name").value;
+  const reminderTime = document.getElementById("reminder-time").value;
+  const reminderList = document.getElementById("reminder-list");
+  const li = document.createElement("li");
+  li.innerText = `${reminderName} - ${reminderTime}`;
+  reminderList.appendChild(li);
+  reminderForm.reset();
+}
